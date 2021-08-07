30f49826d577c98264005fbfb1ddf6ec62d7bec7
LM Bibera <lyndon.bibera@headhuntr.io>
2021-08-07 08:56:35 +0800

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+aws cloudformation create-stack \
+--stack-name test-backend \
+--region us-east-1 \
+--template-body file://backend-server.yaml
---
+++ rollback.sh
@@ -0,0 +1,3 @@
+aws cloudformation delete-stack \
+--stack-name test-backend \
+--region us-east-1
